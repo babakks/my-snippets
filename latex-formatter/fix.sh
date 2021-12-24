783f06b11748452ce56aee9f1e8a3285fe81bb3c
Babak Khalkhali Shandiz <babak.k.shandiz@gmail.com>
2021-12-24 19:16:26 +0330

    🎁 Add snippet for latex-formatter dependencies
    Signed-off-by: Babak Khalkhali Shandiz <babak.k.shandiz@gmail.com>

---
+++ latex-formatter/fix.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/sh
+
+# Below are the missing dependencies that should be met to enable using
+# `latexindent` to format `.tex` files,
+#
+
+apt-get install libyaml-tiny-perl
+apt-get install libfile-homedir-perl
+apt-get install libunicode-utf8-perl
+apt-get install libunicode-linebreak-perl
